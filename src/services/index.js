09861b169b4b6867d262969f5f6dfefb0bb2476c
Aniket Lawaniya <alawaniya3@gmail.com>
2023-07-04 18:07:16 +0530

    Add files via upload

---
+++ src/services/index.js
@@ -0,0 +1,30 @@
+export { getVideos } from "./getVideosService";
+export { getPagedVideosService } from "./getPagedVideoService";
+export { loginService } from "./authServices/loginService";
+export { signupService } from "./authServices/signupService";
+export { getLikesService, removeLikesService, addToLikesService } from "./likes-services";
+export {
+  getPlaylistsService,
+  createPlaylistService,
+  addToPlaylistService,
+  removeFromPlaylistService,
+  removePlaylistService,
+} from "./playlist-services";
+export {
+  getWatchLaterService,
+  addToWatchLaterService,
+  removeFromWatchLaterService,
+} from "./watch-later-services";
+export {
+  getHistoryService,
+  addToHistoryService,
+  removeFromHistoryService,
+  clearHistoryService,
+} from "./history-services";
+export { getCategoriesService } from "./getCategoriesService";
+export {
+  getNotesForVideoService,
+  createNoteService,
+  updateNoteService,
+  deleteNoteService,
+} from "./notesServices";
